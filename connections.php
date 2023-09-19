e7d2d6601c34ac63378f549e588d55772e981d28
Aula72 <simaulat@gmail.com>
2023-09-19 20:02:34 +0300

    mapp-comit

--- connections.php
+++ connections.php
@@ -9,8 +9,8 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=".DB_NAME, $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-  print_r(PDO::getAvailableDrivers());
+  // echo "Connected successfully";
+  // print_r(PDO::getAvailableDrivers());
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
