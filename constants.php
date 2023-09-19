461313baa9c592c6fefc7fbdcbf8d28c17f86444
Aula72 <simaulat@gmail.com>
2023-09-19 18:56:47 +0300

    mapp-comit

--- constants.php
+++ constants.php
@@ -6,8 +6,8 @@
 // define('DB_HOST', 'localhost');
 
 const DB_HOST = "localhost";
-const DB_USER = "keberaorganics_matatu";
+const DB_USER = "keberaorganics_wolfarm";
 const DB_PSW = "(KeBERA6669)";
-const DB_NAME = "keberaorganics_wolfarm";
+const DB_NAME = "keberaorganics_matatu";
 
 define('BASE_URL', $_SERVER['HTTP_HOST']);
