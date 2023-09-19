9313d0d80792101263acf5492ee911a2971fa2c4
Aula72 <simaulat@gmail.com>
2023-09-20 01:26:17 +0300

    mapp-comit

--- views/index.php
+++ views/index.php
@@ -7,10 +7,32 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
     <!-- CSS File -->
     <link rel="stylesheet" href="/assets/styles/index.css" />
-    <title>Youtube Clone with HTML & CSS</title>
+    <title>Matatu</title>
   </head>
   <body>
-      <header class="header">.</header>
+    <!-- header starts -->
+      <header>
+       <div class="logo left">
+         <i id="menu" class="material-icons">menu</i>
+         <img src="https://www.freecodecamp.org/news/content/images/2022/01/yt-logo.png">
+       </div>
+       
+       <div class="search center">
+         <form action="">
+           <input type="text" placeholder="Search" />
+           <button><i class="material-icons">search</i></button>
+         </form>
+         <i class="material-icons mic">mic</i>
+       </div>
+       
+       <div class="icons right">
+         <i class="material-icons">videocam</i>
+         <i class="material-icons">apps</i>
+         <i class="material-icons">notifications</i>
+         <i class="material-icons display-this">account_circle</i>
+       </div>
+      </header>
+  <!-- header ends here -->
       <main>
           <div class="side-bar">.</div>
           <div class="content">.</div>
