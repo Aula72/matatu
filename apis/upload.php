bffad7af81b5bffd3e69ded33cac23e9e132dee5
Aula72 <simaulat@gmail.com>
2023-09-21 01:02:35 +0300

    mapp-comit

--- apis/upload.php
+++ apis/upload.php
@@ -108,11 +108,11 @@ else
   */
 
 
-  $target_dir = "uploads/";
+$target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-die(json_encode($_FILES));
+// die(json_encode($_FILES));
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
--- views/video.php
+++ views/video.php
@@ -8,7 +8,7 @@
 
 <form action="/apis/upload.php" method="post" enctype="multipart/form-data">
 <label for="file"><span>Filename:</span></label>
-<input type="file" name="file" id="file" /> 
+<input type="file" name="fileToUpload" id="fileToUpload" /> 
 <br />
 <input type="submit" name="submit" value="Submit" />
 </form>
