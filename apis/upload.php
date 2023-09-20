7de01bcd9f52e37794a25eeda3e7d47607c433fb
Aula72 <simaulat@gmail.com>
2023-09-21 00:57:23 +0300

    mapp-comit

--- apis/upload.php
+++ apis/upload.php
@@ -145,16 +145,16 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
 }
 
 // Check if $uploadOk is set to 0 by an error
-//if ($uploadOk == 0) {
-  //echo "Sorry, your file was not uploaded.";
+if ($uploadOk == 0) {
+  die("Sorry, your file was not uploaded.");
 // if everything is ok, try to upload file
-//} else {
-  //if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
   move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
-    //echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-  //} else {
-   // echo "Sorry, there was an error uploading your file.";
-  //}
-//}
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    die("Sorry, there was an error uploading your file.");
+  }
+}
 
-header("location: /");
+header("location: /video");
