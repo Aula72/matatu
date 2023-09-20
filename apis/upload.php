9b8415ad0791751073b84f32c8b2219eb02d2fe6
Aula72 <simaulat@gmail.com>
2023-09-21 00:29:03 +0300

    mapp-comit

--- apis/upload.php
+++ apis/upload.php
@@ -12,45 +12,50 @@ header("auth: b37eb06ae70e2daa463988373652af15");
 // die();
 
 // get_token_from_id();
-$allowedExts = array("jpg", "jpeg", "gif", "png", "mp3", "mp4", "wma");
-$extension = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-die(json_encode($_FILES));
-if ((($_FILES["file"]["type"] == "video/mp4")
-|| ($_FILES["file"]["type"] == "audio/mp3")
-|| ($_FILES["file"]["type"] == "audio/wma")
-|| ($_FILES["file"]["type"] == "image/pjpeg")
-|| ($_FILES["file"]["type"] == "image/gif")
-|| ($_FILES["file"]["type"] == "image/jpeg"))
-
-&& ($_FILES["file"]["size"] < 2000000000)
-&& in_array($extension, $allowedExts))
-
-  {
-  if ($_FILES["file"]["error"] > 0)
-    {
-    echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
-    }
-  else
-    {
-    echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-    echo "Type: " . $_FILES["file"]["type"] . "<br />";
-    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
-
-    if (file_exists("uploads/" . $_FILES["file"]["name"]))
-      {
-      echo $_FILES["file"]["name"] . " already exists. ";
-      }
-    else
-      {
-      move_uploaded_file($_FILES["file"]["tmp_name"],
-      "uploads/" . $_FILES["file"]["name"]);
-      echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
-      }
-    }
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
   }
-else
-  {
-  echo "Invalid file";
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
   }
+}
--- views/video.php
+++ views/video.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 
 <head>
-<title></title>
+<title>Upload</title>
 </head>
 
 <body>
 
 <form action="/apis/upload.php" method="post" enctype="multipart/form-data">
 <label for="file"><span>Filename:</span></label>
-<input type="file" name="file" id="file" /> 
+<input type="file" name="fileToUpload" id="fileToUpload" /> 
 <br />
 <input type="submit" name="submit" value="Submit" />
 </form>
