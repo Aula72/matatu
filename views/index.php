1b3977da8b74094bfd436b3f6e187129f738b350
Aula72 <simaulat@gmail.com>
2023-09-21 04:22:05 +0300

    mapp-comit

--- views/index.php
+++ views/index.php
@@ -1,8 +1,15 @@
 <div class="col-md-4">side adss</div>
 <div class="col-md-8" style="height: 75%;">
-            <video    autoplay controls style="height: 80% !important;">
-                    <source src="https://matatu.keberaorganics.com/uploads/user-videos/569e87cb50cb09d7d6ee4a1f46632f06.mp4" type="video/mp4">
-            </video>
+        
+            <script type="text/javascript">
+
+// var nextVideo = "https://matatu.keberaorganics.com/uploads/user-videos/569e87cb50cb09d7d6ee4a1f46632f06.mp4";
+// var videoPlayer = document.getElementById('videoPlayer');
+// videoPlayer.onended = function(){
+//     videoPlayer.src = nextVideo;
+// }
+</script>
+<video id="videoPlayer" src="https://matatu.keberaorganics.com/uploads/avatars/a8b0fab10d5b220e3c7afb0236e926e6.mp4" autoplay autobuffer controls />
         </div>
     </div>
 <div class="row" style="background-color:red; height: 25%;">
@@ -17,3 +24,17 @@
                 overflow: hidden
         }
 </style>
+
+<script> 
+        var nextVideo = ["https://matatu.keberaorganics.com/uploads/user-videos/569e87cb50cb09d7d6ee4a1f46632f06.mp4","https://matatu.keberaorganics.com/uploads/user-videos/2d58a8efd15e9e70569c36a1d9b32cfb.mp4"]; 
+        var curVideo = 0; 
+        var videoPlayer = document.getElementById('videoPlayer'); 
+        videoPlayer.onended = function(){ 
+        if(curVideo == 0){ 
+                videoPlayer.src = nextVideo[1]; 
+                curVideo = 1; 
+        } else if(curVideo == 1){ 
+                videoPlayer.src = nextVideo[0]; curVideo = 0; 
+        } 
+        } 
+</script>
