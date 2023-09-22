cb43cb146895b7cb71e8c0334ac72a69090165da
Aula72 <simaulat@gmail.com>
2023-09-23 02:51:33 +0300

    mapp-comit

--- views/logout.php
+++ views/logout.php
@@ -1,11 +1,21 @@
+<?php
+
+unset($_COOKIE['user_key']);
+?>
 <script type="text/javascript">
 	
 	localStorage.removeItem('auth');
 	localStorage.removeItem('type');
 	localStorage.clear();
+
+	$.ajax({
+		url:`${base_url}/apis/user.php`,
+		method:'POST',
+		headers,
+		success:(data, status)=>{
+			window.location = '/login'
+		}
+	})
 </script>
-<?php
 
-unset($_COOKIE['user_key']);
-
-header('location: /login');
+<!-- header('location: /login'); -->
