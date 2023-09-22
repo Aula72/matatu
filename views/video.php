aeb3eb0b2ce95d222511641e25bfdcfdd24675e5
Aula72 <simaulat@gmail.com>
2023-09-22 23:26:21 +0300

    mapp-comit

--- views/video.php
+++ views/video.php
@@ -9,6 +9,10 @@
 		headers,
 		success:(data, status)=>{
 			console.log(data)
+			let po = ''
+			for(let k of data.videos){
+				po+=`<li>${k.video_name}<button class='btn btn-small btn-danger left'>Remove</button></li>`
+			}
 		}
 	})
 </script>
