6a6f0aa86f1a460beaaecaf8f2e5f8a5911126f0
Aula72 <simaulat@gmail.com>
2023-09-22 20:34:04 +0300

    mapp-comit

--- apis/videos.php
+++ apis/videos.php
@@ -5,19 +5,28 @@ error_reporting(~E_NOTICE);
 include_once "helper.php";
 $method = $_SERVER['REQUEST_METHOD'];
 
+$user_id = get_token_from_id()?get_token_from_id()['user_id']:'';
 switch ($method) {
 	case 'GET':
 		if(isset($_GET['id'])){
 
 		}else{
-			
-			$vid = [];
-			$user_id = get_token_from_id()['user_id'];
-			$v = make_query("select * from videos where user_id=:id",[':id'=>$user_id]);
-			foreach($v->fetchAll(PDO::FETCH_ASSOC) as $video){
-				array_push($vid, $video);
+			if(isset($user_id)){
+				$vid = [];
+			// $user_id = get_token_from_id()['user_id'];
+				$v = make_query("select * from videos where user_id=:id",[':id'=>$user_id]);
+				foreach($v->fetchAll(PDO::FETCH_ASSOC) as $video){
+					array_push($vid, $video);
+				}
+				$msg['videos'] = $vid;
+			}else{
+				$v = make_query("select * from videos");
+				foreach($v->fetchAll(PDO::FETCH_ASSOC) as $video){
+					array_push($vid, $video);
+				}
+				$msg['videos'] = $vid;
 			}
-			$msg['videos'] = $vid;
+			
 		}
 		break;
 	case "POST":
