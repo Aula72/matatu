023874767ec823728134116311e8fa3eb897434d
Aula72 <simaulat@gmail.com>
2023-09-23 04:14:35 +0300

    mapp-comit

--- views/index.php
+++ views/index.php
@@ -203,7 +203,7 @@ float: right;
 
 
 <?php 
-$cv = make_query("select * from ads");
+$cv = make_query("select * from ad");
 foreach($cv->fetchAll() as $c){
 ?>
 <div class="modal fade" id="exampleModal<?php echo $c['id'];?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel${k.id}" aria-hidden="true">
